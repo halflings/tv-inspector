db789a0ef24cdc86bed44bd6916467ae37333950
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-11-06 20:04:59 +0100

    Big changes. Most notably: learning/classification with SVMs

--- features.py
+++ features.py
@@ -1,16 +1,20 @@
-from collections import Counter, defaultdict
+from collections import Counter
 import os
 import re
 import string
 
 import pysrt
 import nltk
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.feature_selection import VarianceThreshold
+from sklearn import cross_validation
+from sklearn import svm
 
 URL_REGEX = '(?:www\.)|(?:https?://)\S+\.\S+'
 STOP_WORDS = set(nltk.corpus.stopwords.words('english')) | {"n't", "..."} | set(string.punctuation)
 STEMMER = nltk.stem.porter.PorterStemmer()
 
-def extract_features(subtitle_text):
+def extract_features(subtitle_lines):
     word_counter = Counter()
     for line in subtitle_lines:
         tokens = nltk.word_tokenize(line)
@@ -40,21 +44,50 @@ def extract_lines(subtitle_path):
         subtitle_lines.append(text)
     return subtitle_lines
 
+def classification_validation(features, labels):
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=0)
+
+    clf = svm.SVC().fit(X_train, y_train)
+
+    print "* Classifier score: {}".format(clf.score(X_test, y_test))
+
+    print "* Predictions:"
+    print "(actual_series : prediction)"
+    for i, features in enumerate(X_test):
+        series = y_test[i]
+        prediction = clf.predict(features)[0]
+        print '"{}" predicted as "{}"'.format(series, prediction)
+
 if __name__ == '__main__':
-    series_list = ['house_of_cards', 'entourage']
-    subtitles = {series: [os.path.join(series, sub) for sub in os.listdir(series) if not sub.startswith('.')]
-                 for series in series_list}
-    features_db = defaultdict(Counter)
+    series_list = ['house_of_cards', 'entourage', 'parks_and_recreation']
+    words_frequencies = []
+    labels = []
+
     for series in series_list:
         print '* {}'.format(series.upper())
+        subtitles = [os.path.join(series, sub) for sub in os.listdir(series) if not sub.startswith('.')]
 
-        for sub_path in subtitles[series]:
+        for sub_path in subtitles:
             print '  . Analyzed subtitle "{}"'.format(sub_path)
             subtitle_lines = extract_lines(sub_path)
-            features_db[series] += extract_features(subtitle_lines)
+            # Some encoding errors can cause no lines to be detected
+            if not subtitle_lines:
+                continue
+            words_frequencies.append(extract_features(subtitle_lines))
+            labels.append(series)
 
-        print
-        print   '! FEATURES:'
-        print
-        print '\n'.join(map(str, features_db[series].most_common(50)))
-        print
+
+    # Vectorizing the word count among all series
+    vectorizer = DictVectorizer()
+    feature_vectors = vectorizer.fit_transform(words_frequencies)
+
+    # Dropping features with low variance
+    MIN_VARIANCE = 0.03
+    variance_threshold = VarianceThreshold(threshold=MIN_VARIANCE)
+    feature_vectors = variance_threshold.fit_transform(feature_vectors)
+
+    # Raw features per series
+    # raw_features = {series: [feature for i, feature in enumerate(feature_vectors.toarray()) if labels[i] == series] for series in series_list}
+
+    # Cross-validation
+    classification_validation(feature_vectors, labels)
